c8ca1582e30eea474c145bb75a325797183298d1
Keith Samson <k.samson@klab.com>
2014-06-04 11:11:35 +0800

    Functions' first commit

---
+++ Functions/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Functions
+//
+//  Created by Keith Samson on 6/4/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("This is a practice of functions in C!\n");
+    return 0;
+}
+
